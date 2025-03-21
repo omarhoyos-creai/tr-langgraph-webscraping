44813efbc473cf6f8cd8aea718bb8007aae71bc9
Omar Hoyos <edgarhoyos@creai.mx>
2025-03-20 19:01:09 -0600

    feat: add multiagent chatbot to write an essay

--- lg-webscraping-demo/agents/agent.py
+++ lg-webscraping-demo/agents/agent.py
@@ -1,23 +1,94 @@
 import os
-from langchain.chains import LLMChain
-from langchain_community.llms.cloudflare_workersai import CloudflareWorkersAI
-from langchain_core.prompts import PromptTemplate
+from langgraph.graph import StateGraph, START, END
+from agents.agent_state import AgentState
+from models.model import CloudflareModel
+from langchain_core.messages import SystemMessage, HumanMessage
+from constants.prompts import PLAN_PROMPT, RESEARCH_PLAN_PROMPT, WRITER_PROMPT, REFLECTION_PROMPT, RESEARCH_CRITIQUE_PROMPT
+from tools.tavily import tavily
+from agents.queries import Queries
+from pprint import pprint
+
+# Get model from Cloudflare WorkersAI
+model = CloudflareModel().model
 
 class Agent:
-    def __init__(self):
-        print(os.getenv("CLOUDFLARE_ACCOUNT_ID"))
-        self.llm = CloudflareWorkersAI(
-            account_id=os.getenv("CLOUDFLARE_ACCOUNT_ID"),
-            api_token=os.getenv("CLOUDFLARE_API_TOKEN"),
-            llm_model = "@cf/deepseek-ai/deepseek-r1-distill-qwen-32b"
+    def __init__(self, checkpointer=None):
+        builder = StateGraph(AgentState)
+        builder.add_node("planner", self.plan_node)
+        builder.add_node("generate", self.generation_node)
+        builder.add_node("reflect", self.reflection_node)
+        builder.add_node("research_plan", self.research_plan_node)
+        builder.add_node("research_critique", self.research_critique_node)
+        builder.set_entry_point("planner")
+        builder.add_conditional_edges(
+            "generate", 
+            self.should_continue, 
+            {END: END, "reflect": "reflect"}
         )
-        self.template = """Human: {question}
+        builder.add_edge("planner", "research_plan")
+        builder.add_edge("research_plan", "generate")
+        builder.add_edge("reflect", "research_critique")
+        builder.add_edge("research_critique", "generate")
+        self.graph = builder.compile(checkpointer=checkpointer)
 
-        AI Assistant:"""
-        self.prompt = PromptTemplate.from_template(self.template)
-
-    def run(self, question):
-        llm_chain = LLMChain(llm=self.llm, prompt=self.prompt)
-        # question = "What is the capital of France?"
-        result = llm_chain.run(question)
-        return result
+    def plan_node(self, state: AgentState):
+        messages = [
+            SystemMessage(content=PLAN_PROMPT), 
+            HumanMessage(content=state['task'])
+        ]
+        response = model.invoke(messages)
+        return {"plan": response}
+    
+    def research_plan_node(self, state: AgentState):
+        queries = model.with_structured_output(Queries).invoke([
+            SystemMessage(content=RESEARCH_PLAN_PROMPT),
+            HumanMessage(content=state['task'])
+        ])
+        content = state["content"] or []
+        for q in queries.queries:
+            response = tavily.search(query=q, max_results=2)
+            for r in response['results']:
+                content.append(r['content'])
+        return {"content": content}
+    
+    def generation_node(self, state: AgentState):
+        content = "\n\n".join(state['content'] or [])
+        user_message = HumanMessage(
+            content=f"{state['task']}\n\nHere is my plan:\n\n{state['plan']}")
+        messages = [
+            SystemMessage(
+                content=WRITER_PROMPT.format(content=content)
+            ),
+            user_message
+            ]
+        response = model.invoke(messages)
+        return {
+            "draft": response.content, 
+            "revision_number": state.get("revision_number", 1) + 1
+        }
+    
+    def reflection_node(self, state: AgentState):
+        messages = [
+            SystemMessage(content=REFLECTION_PROMPT), 
+            HumanMessage(content=state['draft'])
+        ]
+        response = model.invoke(messages)
+        return {"critique": response.content}
+    
+    def research_critique_node(self, state: AgentState):
+        queries = model.with_structured_output(Queries).invoke([
+            SystemMessage(content=RESEARCH_CRITIQUE_PROMPT),
+            HumanMessage(content=state['critique'])
+        ])
+        content = state['content'] or []
+        for q in queries.queries:
+            response = tavily.search(query=q, max_results=2)
+            for r in response['results']:
+                content.append(r['content'])
+        return {"content": content}
+    
+    def should_continue(self, state):
+        if state["revision_number"] > state["max_revisions"]:
+            return END
+        return "reflect"
+    

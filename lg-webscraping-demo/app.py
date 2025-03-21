ce75badee0c23ebe2dbb598de30165d6a44216ff
Omar Hoyos <edgarhoyos@creai.mx>
2025-03-20 19:01:45 -0600

    feat: implements gradio as the UI to interact with the chatbot

--- lg-webscraping-demo/app.py
+++ lg-webscraping-demo/app.py
@@ -2,14 +2,33 @@ import os
 import gradio as gr
 import agents.agent as ag
 from dotenv import load_dotenv
+from langgraph.checkpoint.sqlite import SqliteSaver
+import logging
 
-load_dotenv()
-print(os.getenv("CLOUDFLARE_ACCOUNT_ID"))
-print(os.getenv("CLOUDFLARE_API_TOKEN"))
+_ = load_dotenv()
 
 def get_answer(question):
-    agent = ag.Agent()
-    return agent.run(question)
+    with SqliteSaver.from_conn_string(":memory:") as checkpointer:
+        agent = ag.Agent(checkpointer=checkpointer)
+        thread = {"configurable": {"thread_id": "1"}}
+        results = []
+        for s in agent.graph.stream({
+                'task': question,
+                "max_revisions": 2,
+                "revision_number": 1,
+                "plan": "",
+                "draft": "",
+                "critique": "",
+                "content": [],
+                "revision_number": None,
+                "max_revisions": None,
+            },
+            thread,
+            stream_mode="debug",
+            debug=True
+        ):
+            results.append(s)
+    return results
 
 demo = gr.Interface(
     fn=get_answer,
